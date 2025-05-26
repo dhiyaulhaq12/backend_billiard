d9d01068718eedd6f61d2d32e91bdbc5a2f06879
Akbargunawan <123akbargunawan@gmail.com>
2025-05-26 16:51:45 +0700

    deploy

--- app.py
+++ app.py
@@ -152,6 +152,12 @@ def get_user():
 
     return jsonify({'data': user_data}), 200
 
+@app.route('/', methods=["GET"])
+def hello ():
+    return jsonify({
+        "msg": "API is ready"
+    })
+
 # =================== RUN APP ====================
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
